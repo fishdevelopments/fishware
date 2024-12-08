e8aefd1cabd5c90cc23b1de8ebb99e2a0b8db97c
Milchstern <zweilorbeeren@gmail.com>
2024-12-08 23:00:10 +0100

    Module: Trident boost

--- src/main/kotlin/de/fishdevelopments/fishware/system/manager/impl/ModuleManager.kt
+++ src/main/kotlin/de/fishdevelopments/fishware/system/manager/impl/ModuleManager.kt
@@ -1,6 +1,7 @@
 package de.fishdevelopments.fishware.system.manager.impl
 
 import de.fishdevelopments.fishware.features.module.Module
+import de.fishdevelopments.fishware.features.module.impl.exploit.TridentBoostModule
 import de.fishdevelopments.fishware.features.module.impl.movement.SprintModule
 import de.fishdevelopments.fishware.features.module.impl.visual.GUIModule
 import de.fishdevelopments.fishware.system.manager.ClassInstanceManager
@@ -8,7 +9,18 @@ import de.fishdevelopments.fishware.system.manager.ClassInstanceManager
 class ModuleManager : ClassInstanceManager<Module>() {
 
   override fun run() {
-    this.register(GUIModule::class.java)
+    // combat
+
+    // exploit
+    this.register(TridentBoostModule::class.java)
+    // fun
+
+    // movement
     this.register(SprintModule::class.java)
+
+    // player
+
+    // visual
+    this.register(GUIModule::class.java)
   }
 }
