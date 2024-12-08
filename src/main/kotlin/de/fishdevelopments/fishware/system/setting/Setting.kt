6b65228f1e245293e2a9c1c5487e7f9bf472424c
Milchstern <zweilorbeeren@gmail.com>
2024-12-08 21:43:02 +0100

    Abstract setting

---
+++ src/main/kotlin/de/fishdevelopments/fishware/system/setting/Setting.kt
@@ -0,0 +1,16 @@
+package de.fishdevelopments.fishware.system.setting
+
+import java.util.function.BooleanSupplier
+
+class Setting<T>
+@JvmOverloads
+internal constructor(
+    val name: String,
+    var value: T,
+    var visibleIf: BooleanSupplier = BooleanSupplier { true }
+) {
+  fun setValueAsAny(value: Any) {
+    @Suppress("UNCHECKED_CAST")
+    this.value = value as T
+  }
+}
