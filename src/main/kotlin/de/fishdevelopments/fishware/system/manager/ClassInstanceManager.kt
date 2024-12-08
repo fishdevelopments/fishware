9d5f75ff8702529ee58ca32f95283bf83beaf3c2
Milchstern <zweilorbeeren@gmail.com>
2024-12-08 18:52:22 +0100

    Create ClassInstanceManager.kt

---
+++ src/main/kotlin/de/fishdevelopments/fishware/system/manager/ClassInstanceManager.kt
@@ -0,0 +1,28 @@
+package de.fishdevelopments.fishware.system.manager
+
+abstract class ClassInstanceManager<T : Any> {
+  val instances: MutableMap<Class<out T>, T> = HashMap()
+
+  abstract fun run()
+
+  fun register(clazz: Class<out T>) {
+    this.instances[clazz] = clazz.getConstructor().newInstance()
+  }
+
+  fun register(instance: T) {
+    this.instances[instance::class.java] = instance
+  }
+
+  fun register(clazz: Class<out T>, instance: T) {
+    this.instances[clazz] = instance
+  }
+
+  fun <U : T> get(clazz: Class<U>): U? {
+    @Suppress("UNCHECKED_CAST")
+    return this.instances[clazz] as U?
+  }
+
+  fun getInstances(): Collection<T> {
+    return this.instances.values
+  }
+}
